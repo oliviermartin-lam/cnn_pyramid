8d70b99aaa8c9813ee0bc3714f1f0ce4c9d77828
oliviermartin-lam <olivier.martin@lam.fr>
2020-10-13 21:31:46 +0200

    Add a multi-CPU implementation to speed up the simulation

--- _parFiles/parFileLOOPS.m
+++ _parFiles/parFileLOOPS.m
@@ -12,7 +12,7 @@ nPxPup      = 60; % number of pixels to describe the pupil
 nLenslet    = nPxPup; % number of lenslets for the pyramid
 pyrBinning  = 1;  % binning rate on the pyramid detector
 pyrMod      = 3;  % Modlulation rate on top of the pyramid in lambda/D units
-noiseVar    = 1;  % Noise variance in rd^2
+noiseVar    = 0.01;  % Noise variance in rd^2
 Samp        = 2;  % OVer-sampling factor
 resAO       = nPxPup/2 + 1; % number of pixels to describe the AO-correction area in the PSD
 fovInPixel  = nPxPup*2*Samp; % number of pixel to describe the PSD
@@ -28,16 +28,16 @@ latency     = 1*sampTime; % AO loop latency in seconds;
 L0      = 25;
 
 nr0     = 50;
-r0_min  = 0.01;
-r0_mean = 0.08;
+r0_min  = 0.03;
+r0_mean = 0.12;
 r0_std  = 0.03;
 
 nv      = 50;
 v_min   = 1;
-v_mean  = 10;
-v_std   = 10;
+v_mean  = 5;
+v_std   = 8;
 
 nn      = 50;
 n_min   = 0;
-n_mean  = 0.1;
-n_std   = 1;
+n_mean  = 0.01;
+n_std   = 0.2;
--- scriptGenerateData.m
+++ scriptGenerateData.m
@@ -38,8 +38,26 @@ fxExt = fxExt*fao.fc*fao.nTimes;
 fyExt = fyExt*fao.fc*fao.nTimes;
 index = abs(fxExt)<fao.fc & abs(fyExt)<fao.fc;
 
-% Fitting and aliasing PSD
+% Fitting, Noise and Kolmogorov PSDs
 psdFit = fao.fittingPSD(fao.fx,fao.fy)/fao.atm.r0^(-5/3);
+psdNoise = fao.noisePSD(fao.fx,fao.fy)/fao.noiseVariance;
+
+%mfew parameters to create the phase screen from the PSD
+N       = 4*nPxPup;
+L       = (N-1)*tel.D/(nPxPup-1);
+[fx,fy] = freqspace(N,'meshgrid');
+[~,fr]  = cart2pol(fx,fy);
+fr      = fftshift(fr.*(N-1)/L./2);
+[idx]           = find(fr==0);
+fourierSampling = 1./L;
+
+fc  = fao.fc;
+mskIn = ~(abs(fao.fx)>=fc | abs(fao.fy)>=fc);
+Ts  = fao.samplingTime;
+fxx = fao.fx;
+fyy = fao.fy;
+F = (fao.Rx.*fao.SxAv + fao.Ry.*fao.SyAv);
+psdKolmo = fao.pistonFilter(hypot(fxx,fyy)).*phaseStats.spectrum(hypot(fxx,fyy),fao.atm)/fao.atm.r0^(-5/3);
 
 close all;
 %% CREATING LINEAR HYPERBOLIC SAMPLING FOR INPUTS PARAMETERS - MAXIMIN - TRUNCATED 
@@ -49,7 +67,7 @@ v_list  = lhsdesign_modified(nv,v_min,v_mean + v_std);
 n_list  = lhsdesign_modified(nn,n_min,n_mean + n_std);
 nAll    = nr0*nv*nn
 
-%% LOOP
+%% LOOP - SINGLE CPU
 kIter = 0;
 elapsed = 0;
 
@@ -58,27 +76,26 @@ for iv = 1:nv
     atm.layer.windSpeed = v_list(iv);
     % update the AO controller model
     fao.controller('int');
-    for ir0 = 1:nr0
+    for ir0 = 1:1
         %update the r0 value
         atm.r0 = r0_list(ir0);
         psdServo = fao.anisoServoLagPSD(fao.fx,fao.fy);
-        for in = 1:nn
+        for in = 1:1
             kIter = kIter + 1;
             t_i = tic();
-            % update the noise value
-            fao.noiseVariance = n_list(in);
                         
             % Get the residual phase PSD
             psdAO = psdFit*atm.r0^(-5/3);
-            psdAO(index) = psdAO(index) + reshape(fao.noisePSD(fao.fx,fao.fy) + psdServo,[],1);
+            psdAO(index) = psdAO(index) + reshape(psdNoise*n_list(in) + psdServo,[],1);
             
-            % Get the phase map
-            phaseMap = tools.crop( pupZeroPad .* real( fft2( fftshift( sqrt(psdAO).*randn(size(psdAO)) ) ) ),nPxPup);
-            
+            % Get the phase map                        
+            map = real(ifft2(idx.*sqrt(fftshift(psdAO)).*fft2(randn(atm.rngStream,N))./N).*fourierSampling).*N.^2;
+            phaseMap = map(1:nPxPup,1:nPxPup);
+                
             % propagate through the pyramid
-            ngs.phase = 0;
+            ngs = ngs.*tel;
             ngs.phase = phaseMap;
-            ngs = ngs*tel*pyr;
+            ngs = ngs*pyr;
             
             % record outputs
             fitswrite(single(phaseMap),[path_save,'ground_truth_phase_r0_',num2str(r0_list(ir0)),'_v_',num2str(v_list(iv)),'_noise_',num2str(n_list(in)),'.fits']);
@@ -89,4 +106,102 @@ for iv = 1:nv
         end
     end
 end
+
+
+%% LOOP - MULTI CPU
+% extended list
+r0_list_ext = repelem(r0_list,nv*nn);
+v_list_ext = repelem(repmat(v_list,nr0,1),nn);
+n_list_ext = repmat(n_list,nr0*nv,1);
+
+% init
+nPts    = size(fao.fx,1);
+nTh_    = fao.nTh;
+h1buf   = zeros(nPts,nPts,nTh_);
+h2buf   = zeros(nPts,nPts,nTh_);
+thetaWind   = linspace(0, 2*pi-2*pi/nTh_,nTh_);
+costh       = cos(thetaWind);
+toolboxesList = ver;
+flagControlToolBox = any(strcmp(cellstr(char(toolboxesList.Name)), 'Control System Toolbox'));
+RTF = fao.atf;
+rngStream = atm.rngStream;
+
+addAttachedFiles(gcp,{'telescope.m','telescopeAbstract.m','pyramid.m','source.m'})
+
+t1 = tic();
+
+parfor kIter = 1:100
+    
+    t_i = tic();
+    
+    % Get the fitting PSD
+    r053  = r0_list_ext(kIter)^(-5/3);
+    psdAO = psdFit*r053;
+
+    % Get the noise PSD
+    psdN = psdNoise*n_list_ext(kIter);
+    
+    % Get the servo-lag PSDs        
+    h1buf  = zeros(nPts,nPts,nTh_);
+    h2buf = zeros(nPts,nPts,nTh_);
+    for iTheta = 1:nTh_
+        fi = -v_list_ext(kIter)*fxx*costh(iTheta);
+        id = abs(fi) <1e-7;
+        fi(id) = 1e-8.*sign(fi(id));
+        if flagControlToolBox
+            [MAG, PH] = bode(RTF, 2*pi*fi(:));
+            MAG = reshape(MAG,[nPts,nPts]);
+            MAG(fi == 0) = 1;
+            PH = reshape(PH,[nPts,nPts]);
+            h2buf(:,:,iTheta) = abs(MAG).^2;
+            h1buf(:,:,iTheta) = MAG.*exp(1i*PH/180*pi);
+        else
+            tmp = RTF(exp(-2i*pi*fi*Ts));
+            h1buf(:,:,iTheta) = tmp;
+            h2buf(:,:,iTheta) = abs(tmp).^2;
+        end
+    end
+    h1 = sum(h1buf,3)/nTh_;
+    h2 = sum(h2buf,3)/nTh_;
+            
+    psdServo = (1 + abs(F).^2.*h2 - F.*h1 - conj(F.*h1)).*psdKolmo;
+                
+    % Get the residual phase PSD
+    psdAO(index) = psdAO(index) + reshape(psdN(mskIn) + psdServo(mskIn),[],1);
+    
+    % Get the phase map
+    map = real(ifft2(idx.*sqrt(fftshift(psdAO)).*fft2(randn(rngStream,N))./N).*fourierSampling).*N.^2;
+    phaseMap = map(1:nPxPup,1:nPxPup);
+    
+    % propagate through the pyramid
+    nn = times(ngs,tel);
+    nn.phase = phaseMap;
+    nn = mtimes(nn,pyr);
+    
+    % record outputs
+    fitswrite(single(phaseMap),[path_save,'ground_truth_phase_r0_',num2str(r0_list_ext(kIter)),'_v_',num2str(v_list_ext(kIter)),'_noise_',num2str(n_list_ext(kIter)),'.fits']);
+    fitswrite(single(pyr.camera.frame),[path_save,'measurements_intensity_r0_',num2str(r0_list_ext(kIter)),'_v_',num2str(v_list_ext(kIter)),'_noise_',num2str(n_list_ext(kIter)),'.fits']);
+    elapsed = toc(t_i);
+    fprintf(['Time remaining :',num2str(elapsed*nAll/kIter/3600),' h\n']);   
+end
+tt = toc(t1);
+
+%%
 close all;
+
+figure;
+imagesc(phaseMap);
+pbaspect([1,1,1]);
+colorbar('TickLabelInterpreter','latex')
+xlabel('Pixel in the pupil','interpreter','latex','fontsize',16)
+ylabel('Pixel in the pupil','interpreter','latex','fontsize',16)
+set(gca,'FontSize',16,'FontName','cmr12','TickLabelInterpreter','latex');
+
+figure;
+imagesc(pyr.camera.frame);
+pbaspect([1,1,1]);
+colorbar('TickLabelInterpreter','latex')
+xlabel('Pixel in the WFS detector plane','interpreter','latex','fontsize',16)
+ylabel('Pixel in the WFS detector plane','interpreter','latex','fontsize',16)
+set(gca,'FontSize',16,'FontName','cmr12','TickLabelInterpreter','latex');
+
